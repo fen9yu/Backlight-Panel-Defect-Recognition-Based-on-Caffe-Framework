2a11168eac15aae192db4058c717346100fedbda
fen9yu <fengyu@sjtu.edu.cn>
2016-11-07 18:30:36 +0800

    auto name new models based on the time you run the train

--- caffe/train_Freesense.sh
+++ caffe/train_Freesense.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
+t=`date +%Y%m%d%H%M`
+sed -i "s/\"snapshot_[0-9]*/\"snapshot_$t/" freesense_solver.prototxt
 ~/caffe/caffe/build/tools/caffe train --solver=freesense_solver.prototxt $@
