d7b58f1fa1e64aa0d70322ae5f9835eb08291515
fen9yu <fengyu@sjtu.edu.cn>
2016-11-19 22:20:30 +0800

    change size to 18

--- caffe/create_lmdb.sh
+++ caffe/create_lmdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf img_train_lmdb
 ~/caffe/caffe/build/tools/convert_imageset --shuffle \
---resize_height=64 --resize_width=64 \
+--resize_height=18 --resize_width=18 \
 ~/train/ ~/train/result.txt img_train_lmdb 
 #./train/ is picture dir;
 #train/test_10_25.txt is picture namelist for train_set
@@ -10,6 +10,6 @@ echo "train convert done"
 
 rm -rf img_test_lmdb
 ~/caffe/caffe/build/tools/convert_imageset --shuffle \
---resize_height=64 --resize_width=64 \
+--resize_height=18 --resize_width=18 \
 ~/test/ ~/test/test.txt img_test_lmdb
 echo "test convert done"
