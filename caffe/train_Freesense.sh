aa10c502f51f9e2072c402284025c52dc9518969
fen9yu <fengyu@sjtu.edu.cn>
2016-11-19 22:22:45 +0800

    change default model

--- caffe/train_Freesense.sh
+++ caffe/train_Freesense.sh
@@ -2,5 +2,5 @@
 set -e
 
 t=`date +%Y%m%d%H%M`
-sed -i "s/\"snapshot_[0-9]*/\"snapshot_$t/" freesense_solver.prototxt
-~/caffe/caffe/build/tools/caffe train --solver=freesense_solver.prototxt $@
+sed -i "s/\"snapshot_[0-9]*/\"snapshot_$t/" freesense_solver_11_07.prototxt
+~/caffe/caffe/build/tools/caffe train --solver=freesense_solver_11_07.prototxt $@
