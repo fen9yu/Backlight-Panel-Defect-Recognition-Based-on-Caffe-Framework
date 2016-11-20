b271348a85e468317e62dfc0eb5bcfe312787472
fen9yu <fengyu@sjtu.edu.cn>
2016-11-20 17:10:50 +0800

    remove black part

--- removeblack.py
+++ removeblack.py
@@ -4,19 +4,44 @@ import random
 import cv2
 import os
 import re
+
+def sub_sort(array,low,high):
+    key = img[array[low][0],array[low][1]]
+    while low < high:
+        while low < high and img[array[high][0],array[high][1]] >= key:
+            high -= 1
+        while low < high and img[array[high][0],array[high][1]] < key:
+            img[array[low][0],array[low][1]] = img[array[high][0],array[high][1]]
+            low += 1
+            img[array[high][0],array[high][1]] = img[array[low][0],array[low][1]]
+    img[array[low][0],array[low][1]] = key
+    return low
+
+def quick_sort(array,low,high):
+     if low < high:
+        key_index = sub_sort(array,low,high)
+        quick_sort(array,low,key_index)
+        quick_sort(array,key_index+1,high)
+
+
 k=3
 image="images_a1-1001_5_30_2_2.bmp"
 img=cv2.imread(image,0)
 nonblack=[]
 black=[]
+pixel=[]
 for i in range(len(img)):
 	for j in range(len(img[i])):
 		if img[i][j]>40:
 			nonblack.append(img[i][j])
 		else:
 			black.append([i,j])
+		pixel.append([i,j]);
 temp=sum(nonblack)/len(nonblack)
 for i in black:
 	img[i[0],i[1]]=temp
 	addnoise.pointnoise(i[0],i[1],img,k)
 cv2.imwrite("temp.bmp",img)
+quick_sort(pixel,0,len(pixel)-1)
+for points in pixel:
+	print(img[points[0],points[1]])
