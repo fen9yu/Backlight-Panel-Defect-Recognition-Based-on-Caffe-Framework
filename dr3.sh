bfb03658d5a7e2c363a4004319773eb5a6201c27
fen9yu <fengyu@sjtu.edu.cn>
2016-11-22 02:11:22 +0800

    bug fix

--- dr3.sh
+++ dr3.sh
@@ -1,12 +1,12 @@
 python cut_img.py
 cd caffe
-chmod +x test_Freesense.sh
-./test_Freesense.sh
 cd ../../_a1-1001.bmp
 python ../backlight/caffe/create_test.py
 cd ../backlight/caffe
 chmod +x test_create_lmdb.sh
-./test_create_lmdb.sh
+./test_create_lmdb.sh &> rubbish
+chmod +x test_Freesense.sh
 ./test_Freesense.sh > testresult 2>rubbish
 python classify.py
+rm testresult
 rm rubbish
