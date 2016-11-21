26970faf78d7713ffcbc6bc3defc4816b08235aa
fen9yu <fengyu@sjtu.edu.cn>
2016-11-22 02:16:30 +0800

    add echo message

--- dr3.sh
+++ dr3.sh
@@ -1,12 +1,18 @@
+echo 'Cutting image...'
 python cut_img.py
 cd caffe
 cd ../../_a1-1001.bmp
+echo 'Creating test.txt...'
 python ../backlight/caffe/create_test.py
 cd ../backlight/caffe
 chmod +x test_create_lmdb.sh
+echo 'Creating img_test_lmdb...'
 ./test_create_lmdb.sh &> rubbish
 chmod +x test_Freesense.sh
+echo 'Testing...'
 ./test_Freesense.sh > testresult 2>rubbish
+echo 'Classifying the whole image...'
 python classify.py
+echo 'Cleaning...'
 rm testresult
 rm rubbish
