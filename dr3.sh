975e49ba2706817c6c303e1b9e41cc16a741b365
fen9yu <fengyu@sjtu.edu.cn>
2016-11-22 01:46:34 +0800

    premission fix

--- dr3.sh
+++ dr3.sh
@@ -1,9 +1,11 @@
 python cut_img.py
 cd caffe
+chmod +x test_Freesense.sh
 test_Freesense.sh
 cd ../../_a1-1001.bmp
 python ../backlight/caffe/create_test.py
 cd ../backlight/caffe
+chmod +x test_create_lmdb.sh
 ./test_create_lmdb.sh
 ./test_Freesense.sh > testresult 2>rubbish
 python classify.py
