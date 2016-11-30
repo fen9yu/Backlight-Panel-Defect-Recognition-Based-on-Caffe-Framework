5573638312fcbfb79a33f3cbfb7832cc800722e1
fen9yu <fengyu@sjtu.edu.cn>
2016-11-30 09:42:27 +0800

    find the edge of backlight panel

---
+++ binary_search_edge.py
@@ -0,0 +1,22 @@
+from __future__ import division
+import numpy as np
+import cv2
+
+black=60 #set grey scale under 60 as black
+
+def search_v_edge(center,side,y,img):
+    while(abs(center-side)>1):
+        if img.item(y,(center+side)/2)>black:
+            center=(center+side)/2
+        else:
+            side=(center+side)/2
+    return center
+
+def search_h_edge(center,side,x,img):
+    while(abs(center-side)>1):
+        if img.item((center+side)/2,x)>black:
+            center=(center+side)/2
+
+        else:
+            side=(center+side)/2
+    return center
