4afd059b9a7f40352640e0210d4c58f0453498a0
fen9yu <fengyu@sjtu.edu.cn>
2016-11-30 16:56:32 +0800

    round float into int

--- binary_search_edge.py
+++ binary_search_edge.py
@@ -6,7 +6,7 @@ black=60 #set grey scale under 60 as black
 
 def search_v_edge(center,side,y,img):
     while(abs(center-side)>1):
-        if img.item(y,round((center+side)/2))>black:
+        if img.item(round(y),round((center+side)/2))>black:
             center=(center+side)/2
         else:
             side=(center+side)/2
@@ -14,7 +14,7 @@ def search_v_edge(center,side,y,img):
 
 def search_h_edge(center,side,x,img):
     while(abs(center-side)>1):
-        if img.item(round((center+side)/2),x)>black:
+        if img.item(round((center+side)/2),round(x))>black:
             center=(center+side)/2
 
         else:
