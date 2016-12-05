c20f9a595b38a3f9fe040be47e0208e2c32a9e03
fen9yu <fengyu@sjtu.edu.cn>
2016-12-05 11:11:03 +0800

    add comments

--- mark_border.py
+++ mark_border.py
@@ -14,7 +14,9 @@ RED=[0,0,255]
 
 with open('defects.txt','r') as f:
 	for line in f.readlines():
+		#find the coordinate of the defect
 		m=re.match(r'(.*?)(a.*?)_(\d+)_(\d+)',line)
+		#if it is the first time a defect appear on the picture, make a copy of the picture and rotate it, remove black
 		if not os.path.isfile(m.group(2)+'marked.bmp'):
 			shutil.copy(m.group(2)+'.bmp',m.group(2)+'marked.bmp')
 			orgin=cv2.imread(m.group(2)+'marked.bmp',0)
@@ -31,8 +33,11 @@ with open('defects.txt','r') as f:
 			orgin=cv2.cvtColor(orgin,cv2.COLOR_GRAY2BGR)
 		else:
 			orgin=cv2.imread(m.group(2)+'marked.bmp',1)
+		#get the defect part
 		defect=orgin[side*int(m.group(3))+1:side*(int(m.group(3))+1)-1,side*int(m.group(4))+1:side*(int(m.group(4))+1)-1]
+		#add border to the defect part
 		defect=cv2.copyMakeBorder(defect,1,1,1,1,cv2.BORDER_CONSTANT,value=RED)
+		#put the defect part with border back into the picture
 		orgin[side*int(m.group(3)):side*(int(m.group(3))+1),side*int(m.group(4)):side*(int(m.group(4))+1)]=defect
 
 		cv2.imwrite(m.group(2)+'marked.bmp',orgin)
